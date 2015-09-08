fe0fc2de5cbad1c107484ed7fcbe7948c348c835
Rogerio Hilbert Lima <rogerhil@gmail.com>
2015-09-08 23:21:50 +0100

    Fix js issue.

--- src/static/js/newsletter.js
+++ src/static/js/newsletter.js
@@ -1,48 +1,42 @@
-$(window).load(function () {
-    window.setTimeout(function () {
-        $('object').remove();
-    }, 500);
-
-    $("#form-subscriber").submit(function (e) {
-        e.preventDefault();
-        var email = $(this).find('input[name=email]').val();
-        var data = {
-            csrfmiddlewaretoken: $(this).find('input[name=csrfmiddlewaretoken]').val(),
-            email: email,
-            last_name: $(this).find('input[name=last_name]').val(),
-            first_name: $(this).find('input[name=first_name]').val(),
-        };
-        $('#form-subscriber .errorlist').slideUp();
-        $('#cover').fadeIn();
-        $.ajax({
-            type: 'post',
-            url: $(this).attr('action'),
-            data: data,
-            success: function (data) {
-                $('#cover').fadeOut(function () {
-                    if (!data.success) {
-                        $('#form-subscriber').html(data.html);
-                        $('#form-subscriber .errorlist').hide().slideDown();
-                        $('#form-subscriber .non-field-errors').hide().slideDown();
-                        return;
+$("#form-subscriber").submit(function (e) {
+    e.preventDefault();
+    var email = $(this).find('input[name=email]').val();
+    var data = {
+        csrfmiddlewaretoken: $(this).find('input[name=csrfmiddlewaretoken]').val(),
+        email: email,
+        last_name: $(this).find('input[name=last_name]').val(),
+        first_name: $(this).find('input[name=first_name]').val(),
+    };
+    $('#form-subscriber .errorlist').slideUp();
+    $('#cover').fadeIn();
+    $.ajax({
+        type: 'post',
+        url: $(this).attr('action'),
+        data: data,
+        success: function (data) {
+            $('#cover').fadeOut(function () {
+                if (!data.success) {
+                    $('#form-subscriber').html(data.html);
+                    $('#form-subscriber .errorlist').hide().slideDown();
+                    $('#form-subscriber .non-field-errors').hide().slideDown();
+                    return;
+                }
+                var msg;
+                if (data.s) {
+                    var url = data.success_url + '?s=' + data.s;
+                    if (data.subs) {
+                        url += "&subs=1";
                     }
-                    var msg;
-                    if (data.s) {
-                        var url = data.success_url + '?s=' + data.s;
-                        if (data.subs) {
-                            url += "&subs=1";
-                        }
-                        window.location = url;
-                        msg = 'Redirecionando';
-                    } else {
-                        msg = 'Uma mensagem foi enviada para o email ' + email;
-                    }
-                    $("#form-subscriber").html('<h2 style="color: #A50017">' + msg + '.</h2>').css('padding', '5px');
-                    $("#form-subscriber").effect('highlight', 2000, function () {
-                        $("#form-subscriber").effect('highlight', 2000);
-                    });
+                    window.location = url;
+                    msg = 'Redirecionando';
+                } else {
+                    msg = 'Uma mensagem foi enviada para o email ' + email;
+                }
+                $("#form-subscriber").html('<h2 style="color: #A50017">' + msg + '.</h2>').css('padding', '5px');
+                $("#form-subscriber").effect('highlight', 2000, function () {
+                    $("#form-subscriber").effect('highlight', 2000);
                 });
-            }
-        });
+            });
+        }
     });
 });
